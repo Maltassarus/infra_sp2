462b4a70e1aa02f862d012559d1a012a2c020ccc
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-03-27 23:01:19 +0300

    Small edits

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -68,12 +68,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='db_name'),
+        'USER': os.getenv('POSTGRES_USER', default='test_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='1'),
+        'HOST': os.getenv('DB_HOST', default='test_host'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
