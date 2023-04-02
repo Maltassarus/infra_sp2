0db8312d9a8e19aa67467d1a9d7dbf93f79840ae
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-04-02 17:39:21 +0300

    Корректировка кода согласно правилам оформления

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -121,9 +121,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
  
-#if not DEBUG:
+# if not DEBUG:
 #    STATIC_ROOT = ''
-    
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -5,18 +5,11 @@ from django.db import models
 
 class User(AbstractUser):
 
-<<<<<<< HEAD
     ROLES = (
         ('user', 'Пользователь'),
         ('moderator', 'Модератор'),
         ('admin', 'Администратор')
     )
-=======
-    class Role(models.TextChoices):
-        USER = 'user', ('Пользователь')
-        MODERATOR = 'moderator', ('Модератор')
-        ADMIN = 'admin', ('Администратор')
->>>>>>> 7d9f817691c6611eb87e60d5e77a69843c7166ab
 
     username = models.CharField(
         'username',
@@ -38,30 +31,17 @@ class User(AbstractUser):
     role = models.CharField(
         'Роль',
         max_length=15,
-<<<<<<< HEAD
         default=ROLES[1],
-=======
-        choices=Role.choices,
-        default=Role.USER,
->>>>>>> 7d9f817691c6611eb87e60d5e77a69843c7166ab
     )
     bio = models.TextField('О себе', blank=True)
 
     @property
     def is_admin(self):
-<<<<<<< HEAD
         return self.role == self.ROLES[2]
 
     @property
     def is_moderator(self):
         return self.role == self.ROLES[1]
-=======
-        return self.role == self.Role.ADMIN
-
-    @property
-    def is_moderator(self):
-        return self.role == self.Role.MODERATOR
->>>>>>> 7d9f817691c6611eb87e60d5e77a69843c7166ab
 
     class Meta:
         constraints = [
